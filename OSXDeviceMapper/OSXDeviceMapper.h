d0734233b1559ff6d96042dbe6bbe62b4a4fd342
Michal Parusinski <mparusinski@gmail.com>
2014-07-28 18:54:31 +0100

    Init and free are not required
    Removing unnecessarily redefined methods

--- OSXDeviceMapper/OSXDeviceMapper.h
+++ OSXDeviceMapper/OSXDeviceMapper.h
@@ -1,17 +1,17 @@
 /*
- 
+
  Copyright (c) 2014, Michal Parusinski
  All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
- 
+
  1. Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
  2. Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,11 +22,11 @@
  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  The views and conclusions contained in the software and documentation are those
  of the authors and should not be interpreted as representing official policies,
  either expressed or implied, of the FreeBSD Project.
- 
+
  */
 
 #ifndef _OSX_DEVICE_MAPPER_
@@ -37,19 +37,14 @@
 class com_parusinskimichal_OSXDeviceMapper : public IOService
 {
     OSDeclareDefaultStructors(com_parusinskimichal_OSXDeviceMapper);
-    
+
 public:
-    virtual bool init(OSDictionary *dictionary = 0);
-    
-    virtual void free(void);
-    
     virtual IOService *probe(IOService *provider, SInt32 *score);
-    
+
     virtual bool start(IOService *provider);
-    
+
     virtual void stop(IOService *provider);
-    
+
 };
 
 #endif  // _OSX_DEVICE_MAPPER_
-
