5660a0d000f7b321ee6d422071a1771be9313339
Michal Parusinski <mparusinski@gmail.com>
2014-09-06 16:52:43 +0100

    Improved building script

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ from subprocess import call, check_call, CalledProcessError
 projectName = 'OSXDeviceMapper'
 
 try:
+    print 'BUILDING KEXT'
     check_call(['xcodebuild'])
 except CalledProcessError:
     print "Build not successful"
@@ -13,8 +14,18 @@ except CalledProcessError:
 
 
 try:
+    print 'CHECKING LIBRARIES ARE LINKED'
     check_call(['kextlibs', '-xml', 'build/Debug/'+projectName+'.kext'])
     check_call(['kextlibs', '-xml', 'build/Release/'+projectName+'.kext'])
 except CalledProcessError:
     print "Some libraries has not being linked with kext"
     sys.exit(-1)
+
+
+try:
+    print 'RUNNING DRY SIMULATION'
+    check_call(['sudo', 'cp', '-Rv', 'build/Debug/OSXDeviceMapper.kext',
+        '/tmp'])
+    check_call(['kextutil', '-tn', '/tmp/OSXDeviceMapper.kext'])
+except CalledProcessError:
+    print "Unsuccessful at checking if kext is OK"
