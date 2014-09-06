3adeac3b20e9ec1aa65b34e8d3a7605b5fba9370
Michal Parusinski <mparusinski@gmail.com>
2014-09-06 17:12:40 +0100

    Improved build script to handle configuration mode and move kext to target folder

--- build.py
+++ build.py
@@ -5,18 +5,23 @@ from subprocess import call, check_call, CalledProcessError
 
 projectName = 'OSXDeviceMapper'
 
+mode = 'Release'
+if len(sys.argv) > 1:
+    mode = sys.argv[1]
+destinationFolder = None
+if len(sys.argv) > 2:
+    destinationFolder = sys.argv[2]
+
 try:
     print 'BUILDING KEXT'
-    check_call(['xcodebuild'])
+    check_call(['xcodebuild', '-configuration', mode])
 except CalledProcessError:
     print "Build not successful"
     sys.exit(-1)
 
-
 try:
     print 'CHECKING LIBRARIES ARE LINKED'
-    check_call(['kextlibs', '-xml', 'build/Debug/'+projectName+'.kext'])
-    check_call(['kextlibs', '-xml', 'build/Release/'+projectName+'.kext'])
+    check_call(['kextlibs', '-xml', 'build/'+mode+'/'+projectName+'.kext'])
 except CalledProcessError:
     print "Some libraries has not being linked with kext"
     sys.exit(-1)
@@ -24,8 +29,16 @@ except CalledProcessError:
 
 try:
     print 'RUNNING DRY SIMULATION'
-    check_call(['sudo', 'cp', '-Rv', 'build/Debug/OSXDeviceMapper.kext',
+    check_call(['sudo', 'cp', '-Rv', 'build/'+mode+'/'+projectName+'.kext',
         '/tmp'])
-    check_call(['kextutil', '-tn', '/tmp/OSXDeviceMapper.kext'])
+    check_call(['kextutil', '-tn', '/tmp/'+projectName+'.kext'])
 except CalledProcessError:
     print "Unsuccessful at checking if kext is OK"
+
+if destinationFolder:
+    try:
+        print 'COPYING KEXT TO DESTINATION'
+        check_call(['cp', '-Rv', 'build/'+mode+'/'+projectName+'.kext',
+            destinationFolder])
+    except CalledProcessError:
+        print 'Unsuccessful at moving kext to ' + destinationFolder
